eb05f4f42693275dd1e26bdefb004039d90d3e78
Aditya28-coder <raj713294@gmail.com>
2025-06-30 22:02:41 +0530

    08: Messages Scema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useTRPC } from "@/trpc/client"
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -12,20 +12,20 @@ const Page = () => {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessages = useMutation(trpc.messages.create.mutationOptions({
     onSuccess: () => {
-      toast.success("Background job invoked successfully!");
+      toast.success("Message created successfully!");
     }
   }));
-
-
 
   return ( 
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)}/>
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
-        Invoke bg job
+      <Button disabled={createMessages.isPending} onClick={() => createMessages.mutate({ value: value })}>
+        Create Message
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   )
 }
